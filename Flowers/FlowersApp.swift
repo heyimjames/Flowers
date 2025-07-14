7ccaee13e5f26789d7a157ded30c9c68a2f94d23
james frewin <31506886+heyimjames@users.noreply.github.com>
2025-07-14 19:36:12 +0100

    Initial Commit

---
+++ Flowers/FlowersApp.swift
@@ -0,0 +1,32 @@
+//
+//  FlowersApp.swift
+//  Flowers
+//
+//  Created by James Frewin on 14/07/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlowersApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FlowersTests/FlowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlowersTests.swift
+//  FlowersTests
+//
+//  Created by James Frewin on 14/07/2025.
+//
+
+import Testing
+@testable import Flowers
+
+struct FlowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
